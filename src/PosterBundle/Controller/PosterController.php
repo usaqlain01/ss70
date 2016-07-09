aca71c5488921ec85f6f4a116d4840d36446091f
Usman <usaqlain@caxy.com>
2016-07-09 13:31:35 -0500

    first symfony API endpoint

--- src/PosterBundle/Controller/PosterController.php
+++ src/PosterBundle/Controller/PosterController.php
@@ -10,7 +10,9 @@ namespace PosterBundle\Controller;
 
 
 use AppBundle\Controller\BaseController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class PosterController extends BaseController
 {
@@ -22,5 +24,25 @@ class PosterController extends BaseController
         return $this->render('poster/show.html.twig', [
             'id' => $id,
         ]);
+    }
+
+    /**
+     * @Route("/poster/{id}/replies")
+     * @Method("GET")
+     */
+    public function getRepliesAction()
+    {
+        $replies = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+
+        $data = [
+            'replies' => $replies,
+        ];
+
+        return new Response(json_encode($data));
+
     }
 }
