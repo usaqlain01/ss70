3b0e6a2b5a3e6965a05ca4308c13fd70b59392fe
Usman <usaqlain@caxy.com>
2016-07-09 12:11:07 -0500

    created Poster Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
 
             new AppBundle\AppBundle(),
+            new PosterBundle\PosterBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/PosterBundle/PosterBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: usman
+ * Date: 7/9/16
+ * Time: 12:00 PM
+ */
+
+namespace PosterBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PosterBundle extends Bundle
+{
+}
