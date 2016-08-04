59bcd3ff6406068d076cc0f2946221232ec6caab
Usman <usaqlain@caxy.com>
2016-08-04 11:13:24 -0500

    typo

--- web/js/static_map.js
+++ web/js/static_map.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     function initialize() {
         var mapProp = {
             center:new google.maps.LatLng(lat,lon),
-            zoom:5po,
+            zoom:5,
             mapTypeId:google.maps.MapTypeId.ROADMAP
         };
         var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
